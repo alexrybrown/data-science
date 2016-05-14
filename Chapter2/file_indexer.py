4e9e2ea0d12ffa1c83c6f5aef1da0d5bc8045f21
alexrybrown <alexrybrown@gmail.com>
2016-05-14 10:16:55 -0700

    Finished part of Chapter 2

---
+++ Chapter2/file_indexer.py
@@ -0,0 +1,44 @@
+"""
+This program will index all the files in a user designated directory. It will construct a dictionary where
+they keys are unique words and the values are the files that have those words. This program will pickle the result
+for future use.
+
+Usage: python file_indexer.py [directory]
+"""
+import os
+import pickle
+import re
+import sys
+
+
+def main(directory):
+    # Grab everything from the directory and only keep it if it is a file
+    files = [file for file in os.listdir(directory) if os.path.isfile(file)]
+    pattern = re.compile(r"\w+")
+    pairings = dict()
+    for file in files:
+        with open(file, mode="rb") as f:
+            # If a decoding error occurs just skip the file and got to the next one.
+            try:
+                contents = f.read().decode().lower()
+            except UnicodeDecodeError:
+                print("Error decoding. Skipped {file}.".format(**locals()), file=sys.stderr)
+                continue
+            # We want all the unique words from the file
+            words = list(set(pattern.findall(contents)))
+            # For each word found either append the file to an existing key:value or create a key:value pair.
+            for word in words:
+                if pairings.get(word):
+                    pairings[word].append(file)
+                else:
+                    pairings[word] = [file]
+    # Pickle the pairings for later use
+    with open("file_indexer.pickle", mode="wb") as oFile:
+        pickle.dump(pairings, oFile)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print("Usage: python file_indexer.py [directory]", file=sys.stderr)
---
+++ Chapter2/word_frequency_counter.py
@@ -0,0 +1,32 @@
+"""
+This program will download a web page specified by a user and return the 10 most frequently used words.
+All words will be treated case insensitive. A word is described by the regular expression r"\w+"
+
+Usage: python word_frequency_counter.py [web page]
+"""
+from collections import Counter
+import re
+import sys
+import urllib.request
+
+
+def main(web_page):
+    pattern = re.compile(r"\w+", re.IGNORECASE)
+    try:
+        with urllib.request.urlopen(web_page) as page:
+            # Read, decode, and ignore case of the page
+            html = page.read().decode().lower()
+            # Get a list of all the words
+            words = pattern.findall(html)
+            # Grab only the most common ten words used
+            word_counts = Counter(words).most_common(10)
+            print(word_counts)
+    except:
+        print("Could not open {}".format(web_page), file=sys.stderr)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print("Usage: python word_frequency_counter.py [web page]", file=sys.stderr)
